75012f4120dedb601fe326cb2dc26ab6eeb912c8
Henrique Silva Fernandes <up201106591@fc.up.pt>
2018-02-23 17:32:52 +0000

    0.8.1

--- toolBar/vmdToolBar.tcl
+++ toolBar/vmdToolBar.tcl
@@ -30,7 +30,7 @@ namespace eval toolBar:: {
 		variable nColumns 1; # number of columns per row in the toolbar
 		variable xoff 0	; # coordinates of window
 		variable yoff 0 ; # coordinates of window
-		variable version "0.8"
+		variable version "0.8.1"
 
 		## Packages
 		package require Tk
@@ -59,9 +59,7 @@ proc toolBar::startGui {} {
 	toplevel $toolBar::topGui
 
 	### Hide the Window Decoration
-	wm overrideredirect $toolBar::topGui true
 	wm title $toolBar::topGui "ToolBar" ;# titulo da pagina
-	wm resizable $toolBar::topGui 0 0
 	wm attribute $toolBar::topGui -topmost
 
     #############################################################
@@ -153,6 +151,9 @@ proc toolBar::startGui {} {
 			-borderwidth 0 \
 			-highlightbackground {#575756} \
     	] -in $toolBar::topGui.frame1 -row [expr $row + 1] -column 0 -columnspan 2 -sticky news
+
+	wm overrideredirect $toolBar::topGui true
+	wm resizable $toolBar::topGui 0 0
 
     #############################################################
     #### Trace Variables ########################################
